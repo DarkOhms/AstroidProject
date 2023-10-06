7f3ca0e38b3cb3b3b278c3a40f4ab88928421283
Luke Davis Martin <lukemartin@lukemartinrecords.com>
2023-10-06 14:23:17 -0700

    Created the adapter for the recycler view and did some mild styling

--- app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.udacity.asteroidradar.Asteroid
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
     private val viewModel: MainViewModel by lazy {
-        ViewModelProvider(this).get(MainViewModel::class.java)
+        ViewModelProvider(this)[MainViewModel::class.java]
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +20,37 @@ class MainFragment : Fragment() {
         binding.lifecycleOwner = this
 
         binding.viewModel = viewModel
+
+        val adapter = AsteroidAdapter(AsteroidListener { asteroidId ->
+            viewModel.onAsteroidSelect(asteroidId)
+        })
+
+        binding.asteroidRecycler.adapter = adapter
+
+        /*
+        Here I will attempt to submit a dummy list to test my recycler view
+         */
+        fun generateDummyAsteroidList(): List<Asteroid> {
+            val dummyAsteroidList = mutableListOf<Asteroid>()
+
+            for (i in 1..15) {
+                val asteroid = Asteroid(
+                    id = i.toLong(),
+                    codename = "Asteroid $i",
+                    closeApproachDate = "2023-10-0$i",
+                    absoluteMagnitude = 5.0 + i.toDouble(),
+                    estimatedDiameter = 100.0 + i.toDouble(),
+                    relativeVelocity = 200.0 + i.toDouble(),
+                    distanceFromEarth = 100000.0 + i.toDouble(),
+                    isPotentiallyHazardous = i % 2 == 0
+                )
+                dummyAsteroidList.add(asteroid)
+            }
+
+            return dummyAsteroidList
+        }
+
+        adapter.submitList(generateDummyAsteroidList())
 
         setHasOptionsMenu(true)
 
--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -3,4 +3,7 @@ package com.udacity.asteroidradar.main
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+    fun onAsteroidSelect(asteroidId: Long){
+
+    }
 }
